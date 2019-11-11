88da6d5030c36b50353679f3aa704cf31510d388
domingo.perez3@extendhealth.com <domingo.perez3@extendhealth.com>
2019-11-11 09:20:42 -0700

    Added Command page

---
+++ FindCommandTypes.TraditionalWeb/Controllers/CommandController.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DependenciesFinder;
+using FindCommandTypes.TraditionalWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindCommandTypes.TraditionalWeb.Controllers
+{
+    [Route("command")]
+    public class CommandController : Controller
+    {
+        private readonly CoreCommandsService _coreCommandsService;
+
+        public CommandController() { _coreCommandsService = new CoreCommandsService(new GitHubService()); }
+
+        // GET
+        [HttpGet("{commandName}")]
+        public async Task<IActionResult> Index([FromRoute] string commandName)
+        {
+            var res = await _coreCommandsService
+                          .GetAllCommandsUsedBySSC(Defaults.CommandsDLLPath,
+                                                   x => x.Name.EndsWith("Command")
+                                                        || x.Name.EndsWith("Message"),
+                                                   Defaults.Repositories);
+
+            var found = res.SingleOrDefault(x => x.Name == commandName);
+
+            if (found == null)
+                return NotFound();
+            
+            return View(await CoreCommandVM.fromDependencyAsync(found));
+        }
+    }
+}
