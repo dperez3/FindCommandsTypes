cd7ee3007dc8e64713d99baa47327385d2af3b95
domingo.perez3@extendhealth.com <domingo.perez3@extendhealth.com>
2019-11-11 20:45:38 -0700

    Add Jira subtasks page

--- FindCommandTypes.TraditionalWeb/Controllers/HomeController.cs
+++ FindCommandTypes.TraditionalWeb/Controllers/HomeController.cs
@@ -23,16 +23,44 @@ namespace FindCommandTypes.TraditionalWeb.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var res = await _coreCommandsService.GetAllCommandsUsedBySSC(Defaults.CommandsDLLPath,
-                                                                         x => x.Name.EndsWith("Command") || x.Name.EndsWith("Message"),
-                                                                         Defaults.Repositories);
-            var vms =
-                    (await Task.WhenAll(res.Select(CoreCommandVM.fromDependencyAsync)))
-                    .OrderByDescending(x => x.IsExclusivelyUsedBySSC)
-                    .ThenBy(x => x.Dependencies.Count())
-                    .ThenBy(x => x.Name);
+            var vms = await getCoreCommandsAsync();
             
             return View(vms.ToList());
+        }
+
+        [HttpGet("jira")]
+        public async Task<IActionResult> GetJiraSubTasks()
+        {
+            var sscExclusiveCommands =
+                (await getCoreCommandsAsync())
+                .Where(x => x.IsExclusivelyUsedBySSC);
+
+            var jiraTasks = sscExclusiveCommands
+                .Select(getJiraTask);
+
+            return View(jiraTasks.ToList());
+        }
+
+        private static string getJiraTask(CoreCommandVM command)
+        {
+            return
+                $"- Replace {command.Name} / cfield:\"Assigned Team:@inherit\" / description:\"h1. {command.Name}\n# Confirm this command fits the Req/Resp model.\n ## If it *does NOT*, close this task.\n ## If it *does*, continue to move this out of Core Consumers and into the SSC BFF.\"";
+        }
+
+        private async Task<IEnumerable<CoreCommandVM>> getCoreCommandsAsync()
+        {
+            var res = await _coreCommandsService.GetAllCommandsUsedBySSC(Defaults.CommandsDLLPath,
+                                                                         x => x.Name.EndsWith("Command")
+                                                                              || x.Name.EndsWith("Message"),
+                                                                         Defaults.Repositories);
+
+            var vms =
+                (await Task.WhenAll(res.Select(CoreCommandVM.fromDependencyAsync)))
+                .OrderByDescending(x => x.IsExclusivelyUsedBySSC)
+                .ThenBy(x => x.Dependencies.Count())
+                .ThenBy(x => x.Name);
+
+            return vms;
         }
 
         public IActionResult Privacy() { return View(); }
